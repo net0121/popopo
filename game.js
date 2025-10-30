26ee216a5cc37d50f6abde167fde3a3976d0fa1b
net0121 <72046404+net0121@users.noreply.github.com>
2025-10-29 23:12:34 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 
 const gravity = 0.5;
 const keys = {};
+const camera = { x: 0 };
 
 const kirby = {
   x: 50,
@@ -17,10 +18,25 @@ const kirby = {
   grounded: false
 };
 
+// Level layout based on image
 const platforms = [
-  { x: 0, y: 360, width: 800, height: 40 },
-  { x: 200, y: 300, width: 100, height: 10 },
-  { x: 400, y: 250, width: 100, height: 10 }
+  { x: 0, y: 360, width: 40, height: 40 },
+  { x: 50, y: 320, width: 40, height: 80 },
+  { x: 100, y: 280, width: 40, height: 120 },
+  { x: 150, y: 240, width: 40, height: 160 },
+  { x: 200, y: 200, width: 40, height: 200 },
+  { x: 250, y: 240, width: 40, height: 160 },
+  { x: 300, y: 280, width: 40, height: 120 },
+  { x: 350, y: 320, width: 40, height: 80 },
+  { x: 400, y: 360, width: 40, height: 40 },
+  { x: 450, y: 320, width: 40, height: 80 },
+  { x: 500, y: 280, width: 40, height: 120 },
+  { x: 550, y: 240, width: 40, height: 160 },
+  { x: 600, y: 200, width: 40, height: 200 },
+  { x: 650, y: 240, width: 40, height: 160 },
+  { x: 700, y: 280, width: 40, height: 120 },
+  { x: 750, y: 320, width: 40, height: 80 },
+  { x: 800, y: 360, width: 40, height: 40 }
 ];
 
 document.addEventListener("keydown", (e) => {
@@ -67,10 +83,23 @@ function update() {
       }
     }
   });
+
+  // Camera follows Kirby
+  camera.x = kirby.x - canvas.width / 2 + kirby.width / 2;
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Translate camera
+  ctx.save();
+  ctx.translate(-camera.x, 0);
+
+  // Draw platforms
+  ctx.fillStyle = "green";
+  platforms.forEach((p) => {
+    ctx.fillRect(p.x, p.y, p.width, p.height);
+  });
 
   // Draw Kirby
   ctx.fillStyle = kirby.color;
@@ -78,11 +107,7 @@ function draw() {
   ctx.arc(kirby.x + kirby.width / 2, kirby.y + kirby.height / 2, kirby.width / 2, 0, Math.PI * 2);
   ctx.fill();
 
-  // Draw platforms
-  ctx.fillStyle = "green";
-  platforms.forEach((p) => {
-    ctx.fillRect(p.x, p.y, p.width, p.height);
-  });
+  ctx.restore();
 }
 
 function gameLoop() {
